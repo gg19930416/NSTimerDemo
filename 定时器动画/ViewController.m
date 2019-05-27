c33f2485582c784b867d5f155e0c7940eeaa6cab
guogang123 <843968901@qq.com>
2019-05-27 14:54:44 +0800

    添加进度条

--- 定时器动画/ViewController.m
+++ 定时器动画/ViewController.m
@@ -12,6 +12,7 @@
 @interface ViewController ()
 {
     NSTimer *_timer;
+    CGFloat shortTimeWidth;
 }
 @property (weak, nonatomic) IBOutlet UILabel *percentLabel;
 @property (weak, nonatomic) IBOutlet UIView *groundView;
@@ -22,6 +23,7 @@
 @property (nonatomic,assign) NSUInteger secondNum;
 @property (nonatomic,assign) NSUInteger loopNum;
 @property (nonatomic,strong) NSArray *numArray;
+//@property (nonatomic,assign) CGFloat shortTimeWidth;
 @end
 
 @implementation ViewController
@@ -32,11 +34,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.secondNum = 0;
-    self.loopNum = 0;
-    
-    self.numArray = @[@5,@2];
+
+    self.numArray = @[@5,@2,@5,@3];
+    [self.groundView addSubview:self.animation_view];
     
     _timer = [NSTimer timerWithTimeInterval:1 target:self selector:@selector(timerAction) userInfo:@{} repeats:YES];
     [[NSRunLoop mainRunLoop] addTimer:_timer forMode:NSRunLoopCommonModes];
@@ -63,14 +63,22 @@
 }
 - (IBAction)didClickedStartBtn:(UIButton *)sender {
     //开启定时器
-    [_timer setFireDate:[NSDate distantPast]];
+
+    [UIView animateWithDuration:0.1 animations:^{
+        self.secondNum = 0;
+        self.loopNum = 0;
+        shortTimeWidth = 1;
+        self.animation_view.frame = CGRectMake(0, 0,1, CGRectGetHeight(self.groundView.frame));
+    } completion:^(BOOL finished) {
+        [self->_timer setFireDate:[NSDate distantPast]];
+    }];
 //  self.secondNum = [self.numArray[_loopNum] integerValue];
 }
 
 - (void)timerAction {
     
     if (0 == self.secondNum) {
-        if (self.loopNum == 2 && self.numArray.count == _loopNum &&0 == self.secondNum) {
+        if (self.loopNum == 4 && self.numArray.count == _loopNum &&0 == self.secondNum) {
             [_timer setFireDate:[NSDate distantFuture]];
             return;
         }
@@ -79,16 +87,30 @@
         self.secondNum = [self.numArray[_loopNum] integerValue];
         self.loopNum++;
     }
+    
+    CGFloat width = self.groundView.frame.size.width / 15;
+    shortTimeWidth += width;
+    
+    NSString * percentNum =  [NSString stringWithFormat:@"%f",(shortTimeWidth / self.groundView.frame.size.width)*100];
+    NSLog(@"------ %@   ------ %f", percentNum,shortTimeWidth );
+    self.percentLabel.text = [NSString stringWithFormat:@"%@%%",@(floorf(percentNum.floatValue))];
+    [UIView animateWithDuration:1 animations:^{
+        self.animation_view.frame = CGRectMake(0, 0,self->shortTimeWidth, CGRectGetHeight(self.groundView.frame));
+
+    } completion:^(BOOL finished) {
+        
+    }];
 
     self.secondLabel.text = [NSString stringWithFormat:@"%lus", (unsigned long)self.secondNum];
     self.secondNum --;
-//    NSLog(@"尼玛币 = %ld",self.secondNum);
-//    NSLog(@"循环次数 = %lu",(unsigned long)self.loopNum);
+
 }
+
 
 - (UIView *)animation_view {
     if (!_animation_view) {
         _animation_view = [[UIView alloc] init];
+        _animation_view.backgroundColor = [UIColor blueColor];
     }
     return _animation_view;
 }
